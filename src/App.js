1d8ce8c1588d35c3a9f677e12dbc214fbf956d5f
Nagib <n.s.twahir@student.tue.nl>
2021-04-28 19:28:42 +0200

    trying to set up REACT

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import NodeGraph from './NodeGraph'
+import { csv } from 'd3'
+
+const url = 'https://raw.githubusercontent.com/NTwahir/2ioa0group24/master/data/enron-v1.csv'
+
+function App() {
+  const [data, setData] = useState(["placeholder data"])
+  
+  // csv(url).then(data => {
+  //   console.log(data);
+  //   setData(data);
+  // });
+  
+  return (
+    <>
+    <div>{data}</div>
+    <NodeGraph />
+    </>
+  )
+}
+
+export default App;
