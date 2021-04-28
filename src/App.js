79cf48b6596c9a4efd1c7d4023f954257bc0f4c8
Nagib <n.s.twahir@student.tue.nl>
2021-04-28 20:41:39 +0200

    able to display loaded data on html page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NodeGraph from './NodeGraph'
 import { csv } from 'd3'
 
@@ -7,15 +7,17 @@ const url = 'https://raw.githubusercontent.com/NTwahir/2ioa0group24/master/data/
 function App() {
   const [data, setData] = useState(["placeholder data"])
   
-  csv(url).then(data => {
-    console.log(data);
-    setData(data);
-  });
-  
+  // Determines how often the dataset gets requested.
+  useEffect(() => {
+    // Load the csv dataset
+    csv(url).then(setData);
+  }, []);
+
+  if(!data) return <div>Loading...</div>;
+
   return (
     <>
-    <div>{data}</div>
-    <NodeGraph />
+    <NodeGraph data={data}/>
     </>
   )
 }
--- src/NodeGraph.js
+++ src/NodeGraph.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function NodeGraph() {
+export default function NodeGraph({ data }) {
+    let Array = [1,2,3]
+    // data[0].forEach(e => {
+    //     Array.push(e)
+    // })
+
+
+
     return (
         <div>
-            Hello World!
+            {Array}| | 
+            {data[0].fromId}| | 
+            {console.log(data)}
         </div>
     )
 }
