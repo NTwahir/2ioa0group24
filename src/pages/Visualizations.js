4964660ad716f4c0f686079f7c148a5b08e4918d
Nagib <n.s.twahir@student.tue.nl>
2021-05-05 13:19:29 +0200

    Fixed node link graph. loads at bottom of page. rewrite into jsx.

--- src/pages/Visualizations.js
+++ src/pages/Visualizations.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NodeGraph } from '../NodeGraph';
+import NodeLink from '../NodeLink';
 
 const Visualizations = ({ data }) => {
     return (
         <div>
             <h1>Bar Chart of E-mails sent per user</h1>
             <NodeGraph data={data}/>
+            <NodeLink data={data}/>
         </div>
     )
 }
