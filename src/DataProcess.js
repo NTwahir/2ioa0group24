a2b5c26d9ac2c6feaa9fe1194cad126ce9a8a477
Nagib <n.s.twahir@student.tue.nl>
2021-05-10 13:23:59 +0200

    Filtered dataset, but it is hardcoded for now.

--- src/DataProcess.js
+++ src/DataProcess.js
@@ -5,31 +5,33 @@ import { ascending, sum } from 'd3';
 const DataProcess = (data) => {
 
     // Parse the data if needed
+    data = data.filter(v => 
+        v.toId !== '136' &&
+        v.toId !== '78')
 
 
     // something with sentiment
     // average sentiment per email sent
+    let groupById = data.filter(v => v.fromId === String(96) && v.messageType === "TO");
+    let averageSentiment = sum(groupById, v => v.sentiment) / groupById.length;
     // 10 most negative sent messages
 
     // Group by message sent per user
-    let groupById = data.filter(v => v.fromId === String(96) && v.messageType === "TO");
-    let averageSentiment = sum(groupById, v => v.sentiment) / groupById.length;
     let uniqueNodes = nest().key(d => d.fromId).sortKeys(ascending).entries(data);
     let nodes = [];
     let links = [];
 
-    let id = 1;
     uniqueNodes.forEach(n => {
         nodes.push({"name": n.key});
         n.values.forEach(v => {
-            links.push({"source": v.fromId, "target": "10"});
+            links.push({"source": v.fromId, "target": v.toId});
         });
     })
-    let processedData = {"nodes": nodes, "links": links}
+    
+    let processedData = {"nodes": nodes, "links": links};
 
     // Console debug
     console.log(processedData);
-    console.log(uniqueNodes);
 
     return [uniqueNodes, processedData];
 }
--- src/NodeLink.js
+++ src/NodeLink.js
@@ -2,117 +2,12 @@ import React from 'react';
 import { forceLink, forceManyBody, select, forceSimulation, forceCenter } from 'd3';
 import DataProcess from './DataProcess';
 
-const url = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json";
 
 // set the dimensions and margins of the graph
 const 
 margin = {top: 10, right: 30, bottom: 30, left: 40},
-width = 1000 - margin.left - margin.right,
-height = 1000 - margin.top - margin.bottom;
-
-let data1 = 
-{
-    "nodes": [
-      {
-        "id": 1,
-        "name": "Andy",
-        "gender": "male"
-      },
-      {
-        "id": 2,
-        "name": "Betty",
-        "gender": "female"
-      },
-      {
-        "id": 3,
-        "name": "Cate",
-        "gender": "female"
-      },
-      {
-        "id": 4,
-        "name": "Dave",
-        "gender": "male"
-      },
-      {
-        "id": 5,
-        "name": "Ellen",
-        "gender": "female"
-      },
-      {
-        "id": 6,
-        "name": "Fiona",
-        "gender": "female"
-      },
-      {
-        "id": 7,
-        "name": "Garry",
-        "gender": "male"
-      },
-      {
-        "id": 8,
-        "name": "Holly",
-        "gender": "female"
-      },
-      {
-        "id": 9,
-        "name": "Iris",
-        "gender": "female"
-      },
-      {
-        "id": 10,
-        "name": "Jane",
-        "gender": "female"
-      }
-    ],
-    "links": [
-      {
-        "source": 1,
-        "target": 2
-      },
-      {
-        "source": 1,
-        "target": 5
-      },
-      {
-        "source": 1,
-        "target": 6
-      },
-  
-      {
-        "source": 2,
-        "target": 3
-      },
-      {
-        "source": 2,
-        "target": 7
-      }
-    ,
-  
-      {
-        "source": 3,
-        "target": 4
-      },
-      {
-        "source": 8,
-        "target": 3
-      }
-    ,
-      {
-        "source": 4,
-        "target": 5
-      }
-    ,
-  
-      {
-        "source": 4,
-        "target": 9
-      },
-      {
-        "source": 5,
-        "target": 10
-      }
-    ]
-  };
+width = 1920 - margin.left - margin.right,
+height = 1080 - margin.top - margin.bottom;
 
 const NodeLink = (container, data) => {
     let data2 = DataProcess(data)[1];
@@ -149,7 +44,7 @@ const NodeLink = (container, data) => {
                 .id(d => { return d.name; })                     // This provide  the id of a node
                 .links(data2.links)                                    // and this the list of links
         )
-        .force("charge", forceManyBody().strength(-100))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+        .force("charge", forceManyBody().strength(-200))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
         .force("center", forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
         .on("end", ticked);
 
