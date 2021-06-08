d719dfb419a0f0e1b7185c62395221f5743cce0f
Nagib <n.s.twahir@student.tue.nl>
2021-06-08 15:35:11 +0200

    2 vis on 1 page

--- src/pages/Visualizations2.js
+++ src/pages/Visualizations2.js
@@ -4,6 +4,8 @@ import { Graph } from '../Graph';
 const Visualizations2 = ({ data }) => {
     return (
         <div>
+            <h1>Node-edge graph of email network</h1>
+            <Graph data={data} type={1}/>
             <h1>Chord graph of email network</h1>
             <Graph data={data} type={2}/>
         </div>
