8aea8f5c8886ef748b5ff139509d2c568941cd19
Nagib <n.s.twahir@student.tue.nl>
2021-07-02 15:22:33 +0200

    pathing

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -9,9 +9,9 @@ const NavBar = () => {
     return (
         <div className={navbar}>
                 <Nav>
-                    <Nav.Item to={"/"} value="Homepage" icon="home" />
-                    <Nav.Item to={"/About"} value="About Us" icon="globe" />
-                    <Nav.Item to={"/Visualizations"} value="Visualizations" icon="star" />
+                    <Nav.Item to={process.env.PUBLIC_URL + "/"} value="Homepage" icon="home" />
+                    <Nav.Item to={process.env.PUBLIC_URL + "/About"} value="About Us" icon="globe" />
+                    <Nav.Item to={process.env.PUBLIC_URL + "/Visualizations"} value="Visualizations" icon="star" />
                     <Nav.Item to="https://github.com/NTwahir/2ioa0group24" value="GitHub" icon="github" />
                 </Nav>
         </div>
